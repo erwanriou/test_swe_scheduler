f10ba57fc049f1a7140b9853a70f5be3a3ee55e2
erwanriou <rr.1.riou@gmail.com>
2026-01-18 14:37:43 +0100

    Add template for scheduler

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require("./app")
+// TODO const transaction = require("./services/transactions")
+
+// CONNECT DATABASE
+// TODO transaction("Scheduler")
+
+// LISTEN APP
+app.listen(3000, () => console.log("Architect listening on port 3000!"))
